c445a90df62c6db0efec9ce0123c00406421bc06
Imran ahmad mir <letsmeetimran@gmail.com>
2025-09-04 22:42:05 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,5 @@
+// Show project demo
+function showProject(id) {
+  document.querySelectorAll(".demo").forEach((d) => (d.style.display = "none"));
+  document.getElementById(id).style.display = "block";
+}
